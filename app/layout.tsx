3144204ad10001f9a6e1af42235dcf8a9ec23f39
tusin777 <ruslantusin@gmail.com>
2025-04-07 00:14:12 +0200

    test3

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>
-        ok uhyuyuyiuhxcxcxcuyuyu7y{children}
-      </body>
+      <body className={`${inter.className} antialiased`}>test1{children}</body>
     </html>
   );
 }
